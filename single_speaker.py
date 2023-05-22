0a3d69b3e454289c7a4919d8d2438afb7e167150
thanhhong <64731516+nongthanhhong@users.noreply.github.com>
2023-05-22 12:11:17 +0700

    .

--- single_speaker.py
+++ single_speaker.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
 
     path_to_model = args.path_to_model
 
-    _ = utils.load_checkpoint(path_to_model, net_g, None)
+    net_g, _, _, _ = utils.load_checkpoint(path_to_model, net_g, None)
 
 
     text = args.text
